805968b312d04a962e957262475d37222dfbf388
Ankush Gupta <ankush.me@hotmail.com>
2012-08-24 16:18:25 -0700

    surface normals done.

--- color_association/src/associate.cpp
+++ color_association/src/associate.cpp
@@ -12,14 +12,21 @@
 #include <math.h>
 #include <sstream>
 
-#include <pcl/point_types.h>
-#include <pcl/features/normal_3d.h>
+//#include <pcl/point_types.h>
+#include <pcl/search/organized.h>
+#include <pcl/features/integral_image_normal.h>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 
+/** Create a shared pointer type for pcl::Normal.*/
+typedef boost::shared_ptr<pcl::Normal> NormalPtr;
+typedef pcl::IntegralImageNormalEstimation<pcl::PointXYZRGB, pcl::Normal> NormalEstimation;
+typedef boost::shared_ptr<NormalEstimation> NormalEstimationPtr;
+
+/** Structure to store HSV values. */
 struct HSV {
   uint8_t h,s,v;
   typedef boost::shared_ptr<HSV> Ptr;
@@ -34,6 +41,7 @@ struct HSV {
 };
 
 
+/** Structure to store RGB values. */
 struct RGB {
   uint8_t r,g,b;
   typedef boost::shared_ptr<RGB> Ptr;
@@ -78,7 +86,7 @@ struct HSVInfo {
   }
 };
 
-
+/** Converts opencv RGB values to HSV. */
 HSV RGB_to_HSV(uint8_t r, uint8_t g, uint8_t b) {
   uint8_t pixel_data[] = {r,g,b};
   cv::Mat pixel(1,1,CV_8UC3, (void*) pixel_data);
@@ -89,6 +97,7 @@ HSV RGB_to_HSV(uint8_t r, uint8_t g, uint8_t b) {
 	     pixelHSV.at<uint8_t>(2));
 }
 
+/** Converts opencv HSV values to RGB. */
 RGB HSV_to_RGB(uint8_t h, uint8_t s, uint8_t v) {
   uint8_t pixel_data[] = {h,s,v};
   cv::Mat pixel(1,1,CV_8UC3, (void*) pixel_data);
@@ -100,6 +109,7 @@ RGB HSV_to_RGB(uint8_t h, uint8_t s, uint8_t v) {
 }
 
 
+/** Returns the 'distance' b/w openCV hue values.*/
 uint8_t hue_dist(uint8_t hue1, uint8_t hue2) {
   int delta = 2 * abs(((int)hue1) - ((int)hue2));
   return (uint8_t) ((( abs(360 - delta) < delta ) ? abs(360-delta) : delta) / 2);
@@ -120,14 +130,14 @@ get_hue_clusters(pcl::PointXYZRGB seed, int seed_idx,
   base_cluster->push_back(seed_idx);
   clusters.push_back(base_cluster);
 
-  for (int i=0; i < indices.size(); i += 1) {
+  for (unsigned int i=0; i < indices.size(); i += 1) {
     pcl::PointXYZRGB neighbor_pt = cloud->at(indices[i]);
     HSV neighborHSV = RGB_to_HSV(neighbor_pt.r, neighbor_pt.g, neighbor_pt.b);
 
     /** Find the closest cluster to this neighbor pt. */
     int closest_cluster_idx = -1;
     int closest_cluster_dist = 500;
-    for(int j=0; j < clusters.size(); j+=1) {
+    for(unsigned int j=0; j < clusters.size(); j+=1) {
       pcl::PointXYZRGB rep_pt = cloud->at(clusters.at(j)->at(0));
       HSV repHSV = RGB_to_HSV(rep_pt.r, rep_pt.g, rep_pt.b);     
       uint8_t hueDist = hue_dist(repHSV.h, neighborHSV.h);
@@ -149,6 +159,8 @@ get_hue_clusters(pcl::PointXYZRGB seed, int seed_idx,
 }
 
 
+/** Displays the color corresponding to the H,S,V
+    values in a small window. */
 void  display_HSV(uint8_t h, uint8_t s, uint8_t v) {
   RGB rgb = HSV_to_RGB(h,s,v);
 
@@ -164,7 +176,9 @@ void  display_HSV(uint8_t h, uint8_t s, uint8_t v) {
 }
 
 
-
+/** Averages the HSVINFO in the input vector.
+    The output HSV values are mean of the input.
+    The output standard deviations are the max of the input. */
 HSVInfo::Ptr average_HSV_info(std::vector<HSVInfo::Ptr> hsv_input) {
   int h_total, s_total, v_total;
   h_total = s_total = v_total = 0;
@@ -172,7 +186,7 @@ HSVInfo::Ptr average_HSV_info(std::vector<HSVInfo::Ptr> hsv_input) {
   float h_std, s_std, v_std;
   h_std = s_std = v_std = 0.0;
 
-  for(int i = 0; i < hsv_input.size(); i+=1) {
+  for(unsigned int i = 0; i < hsv_input.size(); i+=1) {
     h_total += (int) hsv_input[i]->h;
     s_total += (int) hsv_input[i]->s;
     v_total += (int) hsv_input[i]->v;
@@ -191,6 +205,7 @@ HSVInfo::Ptr average_HSV_info(std::vector<HSVInfo::Ptr> hsv_input) {
 				  h_std, s_std, v_std));
 }
 
+
 /** Returns a cummulative [average] HSV, given a list of
     INDICES of the points in the CLOUD, over which the average
     needs to be taken. */
@@ -200,7 +215,7 @@ HSVInfo::Ptr get_HSV_info(boost::shared_ptr<std::vector<int> > indices,
     int h_total, s_total, v_total;
     h_total = s_total = v_total = 0;
 
-    for(int i=0; i < indices->size(); i+=1) {
+    for(unsigned int i=0; i < indices->size(); i+=1) {
       pcl::PointXYZRGB pt = cloud->at(indices->at(i));
       HSV hsv = RGB_to_HSV(pt.r, pt.g, pt.b);
 
@@ -214,7 +229,7 @@ HSVInfo::Ptr get_HSV_info(boost::shared_ptr<std::vector<int> > indices,
 
     float h_std1, s_std, v_std;
     h_std1 = s_std = v_std = 0.0;
-    for(int i=0; i<indices->size(); i+=1) {
+    for(unsigned int i=0; i<indices->size(); i+=1) {
       pcl::PointXYZRGB pt = cloud->at(indices->at(i));
       HSV hsv = RGB_to_HSV(pt.r, pt.g, pt.b);
       int h = 2 * ((int) hsv.h);
@@ -228,7 +243,7 @@ HSVInfo::Ptr get_HSV_info(boost::shared_ptr<std::vector<int> > indices,
 
 
     h_total = 0;
-    for(int i=0; i < indices->size(); i+=1) {
+    for(unsigned int i=0; i < indices->size(); i+=1) {
       pcl::PointXYZRGB pt = cloud->at(indices->at(i));
       HSV hsv = RGB_to_HSV(pt.r, pt.g, pt.b);
       h_total += (2 * ((int) hsv.h) + 180) % 360;
@@ -236,7 +251,7 @@ HSVInfo::Ptr get_HSV_info(boost::shared_ptr<std::vector<int> > indices,
 
     float h_mean2 = ((float)h_total)/indices->size();
     float h_std2 = 0.0;
-    for(int i=0; i<indices->size(); i+=1) {
+    for(unsigned int i=0; i<indices->size(); i+=1) {
       pcl::PointXYZRGB pt = cloud->at(indices->at(i));
       HSV hsv = RGB_to_HSV(pt.r, pt.g, pt.b);
       int h = (2 * ((int) hsv.h) + 180) % 360;
@@ -259,7 +274,6 @@ HSVInfo::Ptr get_HSV_info(boost::shared_ptr<std::vector<int> > indices,
     uint8_t mean_hue = (uint8_t) (h_mean/2.0);
     HSVInfo::Ptr hsv_info(new HSVInfo(mean_hue, (uint8_t) s_mean, (uint8_t) v_mean,
 				      h_std, s_std, v_std));
-    //display_HSV(hsv_info->h,hsv_info->s,hsv_info->v);
     return hsv_info;
   } else {
     HSVInfo::Ptr hsv_info(new HSVInfo);
@@ -289,7 +303,7 @@ std::vector<HSVInfo::Ptr> get_hole_HSV(pcl::search::OrganizedNeighbor<pcl::Point
   pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud = org_search->getInputCloud();
   std::vector<HSVInfo::Ptr> hsv_info;
 
-  for (int i = 0; i < holes.size(); i += 1) {
+  for (unsigned int i = 0; i < holes.size(); i += 1) {
     surgical_msgs::Hole hole = holes[i];
 
     std::vector<int> neighbors;
@@ -303,7 +317,7 @@ std::vector<HSVInfo::Ptr> get_hole_HSV(pcl::search::OrganizedNeighbor<pcl::Point
 			 neighbors, cloud, hue_thresh);
       int max_cluster_size = -1;
       int max_cluster_idx  = -1;
-      for(int i = 0; i < hue_clusters.size(); i += 1) {
+      for(unsigned int i = 0; i < hue_clusters.size(); i += 1) {
 	if (((int) hue_clusters[i]->size()) > max_cluster_size) {
 	  max_cluster_size = hue_clusters[i]->size();
 	  max_cluster_idx = i;
@@ -316,13 +330,20 @@ std::vector<HSVInfo::Ptr> get_hole_HSV(pcl::search::OrganizedNeighbor<pcl::Point
   return hsv_info;
 }
 
+
+/** Returns a vector of HSVInfo for all the cuts in CUTS.
+    ORG_SEARCH : is the organizedNeighbor data-structure to be
+                 used for nearest neighbor searches in an organized
+		 point-cloud.
+    RADIUS     : The radius [meters] to search for neighbors in.
+    HUE_THRESH : The tolerance for putting hue values in the same bin. */   
 std::vector<HSVInfo::Ptr> get_cut_HSV(pcl::search::OrganizedNeighbor<pcl::PointXYZRGB>::Ptr org_search,
 				      std::vector<surgical_msgs::Cut> cuts,
 				      float radius=0.01, uint8_t hue_thresh=5) {
   pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud = org_search->getInputCloud();
   std::vector<HSVInfo::Ptr> cuts_hsv_info;
 
-  for (int i = 0; i < cuts.size(); i += 1) {
+  for (unsigned int i = 0; i < cuts.size(); i += 1) {
     surgical_msgs::Cut cut = cuts[i];
     std::vector<surgical_msgs::Hole> nodes = cut.nodes;
     std::vector<HSVInfo::Ptr> nodes_hsv_info = get_hole_HSV(org_search, nodes,
@@ -335,14 +356,69 @@ std::vector<HSVInfo::Ptr> get_cut_HSV(pcl::search::OrganizedNeighbor<pcl::PointX
 }
 
 
+/** Finds the surface normal around a point. 
+    X_IDX, Y_IDX : The x and y indices of the point at which
+                   the normal is to be found in the organized point-cloud.
+    NORMAL_ESTIMATOR : A normal_estimator structure appropriately
+                       initialized for finding normals.
+    [see:
+    http://www.pointclouds.org/documentation/tutorials/normal_estimation_using_integral_images.php ]*/
+NormalPtr get_point_normal(const int x_idx, const int y_idx,
+			   NormalEstimationPtr normal_estimator) {
+  int pt_idx = x_idx*normal_estimator->getInputCloud()->width + y_idx;
+  pcl::Normal normal;
+  normal_estimator->computePointNormal(x_idx, y_idx, pt_idx, normal);
+
+  NormalPtr ret_normal(new pcl::Normal);
+  *ret_normal  = normal;
+  return ret_normal;
+}
+
+
+/** Returns the surface normal a the HOLE. */
+NormalPtr get_hole_normal(const surgical_msgs::Hole &hole,
+			  NormalEstimationPtr normal_estimator) {
+  return get_point_normal(hole.x_idx, hole.y_idx, normal_estimator);
+}
+
+/** Returns a vector of surface normals for each node in every cut inside CUTS.*/
+std::vector<boost::shared_ptr<std::vector<NormalPtr> > >
+get_cuts_normal(const std::vector<surgical_msgs::Cut> cuts,
+		NormalEstimationPtr normal_estimator) {
+  std::vector<boost::shared_ptr<std::vector<NormalPtr> > > normals;
+  for (unsigned int i = 0; i < cuts.size(); i+= 1) {
+    surgical_msgs::Cut cut = cuts[i];
+    boost::shared_ptr<std::vector<NormalPtr> > cut_normals(new std::vector<NormalPtr>); 
+    for(unsigned int j =0; j < cut.nodes.size(); j += 1) {
+      cut_normals->push_back(get_hole_normal(cut.nodes[j], normal_estimator));
+    }
+    normals.push_back(cut_normals);
+  }
+  return normals;
+}
+
+
+/** The callback to handle new surgical::initInfo messages.
+
+    1. Detects hole and cut colors.
+    2. Finds the local surface normal directions at the holes and the cuts. */
 void initInfoCB(const surgical_msgs::InitInfo::ConstPtr& info) {
   ROS_INFO("Initinfo recieved.");
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_pcl (new pcl::PointCloud<pcl::PointXYZRGB>);
   pcl::fromROSMsg(info->cloud, *cloud_pcl);
-  
+
+  // Initialize organized neighbor structure for nearest neighbor search
   pcl::search::OrganizedNeighbor<pcl::PointXYZRGB>::Ptr
     org_search(new pcl::search::OrganizedNeighbor<pcl::PointXYZRGB>);
   org_search->setInputCloud(cloud_pcl);
+
+  // Initialize normal-estimation structure
+  NormalEstimationPtr normal_estimator(new NormalEstimation);
+  normal_estimator->setNormalEstimationMethod (normal_estimator->AVERAGE_3D_GRADIENT);
+  normal_estimator->setMaxDepthChangeFactor(0.02f);
+  normal_estimator->setNormalSmoothingSize(10.0f);
+  normal_estimator->setInputCloud(cloud_pcl);
+
   std::vector<surgical_msgs::Hole> holes = info->holes;
   std::vector<HSVInfo::Ptr> hole_hsv_info = get_hole_HSV(org_search, holes);
 
