135eb2d03c78726bfb0e89e565082188d6ddd68a
Ankush Gupta <ankush.me@hotmail.com>
2012-09-13 23:09:26 -0700

    and -images : needle mask obtained.

--- and_images/src/and_images.cpp
+++ and_images/src/and_images.cpp
@@ -20,13 +20,14 @@
 
 using namespace cv;
 
-ImageProcessor::Ptr cannyblur(new CannyBlur);
-ImageAND ander(cannyblur,3);
-HueFilter hFilter(75, 85);
-SaturationFilter sFilter(50, 255);
-
-/** Class for finding a needle (macro sized, circular) in an organized point cloud. */
+/** Class for finding a needle (macro sized, circular)
+    in an organized point cloud. */
 class NeedleFinder : CloudImageComm {
+
+  ImageProcessor::Ptr cannyblur;
+  ImageAND ander;
+  HueFilter hFilter;
+  SaturationFilter sFilter;
 
   /** This is called whenever a new point-cloud is recieved. */
   void process() {
@@ -40,42 +41,52 @@ class NeedleFinder : CloudImageComm {
       cv::HoughCircles(debug_mat, circles, CV_HOUGH_GRADIENT,
 		       2, debug_mat.rows/3, 200, 100,50,75);
 
-      cv::Mat circular_mask = cv::Mat::zeros(_img_cv.size(), _img_cv.type());
+      cv::Mat circular_mask = cv::Mat::zeros(_img_cv.size(), CV_8UC1);
       for( size_t i = 0; i < circles.size(); i+=1 ) {
 	cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
 	int radius = cvRound(circles[i][2]);
 	// Draw the circle outline
-	cv::circle(circular_mask, center, radius, 255, 7, 8, 0);
+	cv::circle(circular_mask, center, radius, 255, 10, 8, 0);
       }
 
       cv::Mat hue_mask, sat_mask, color_mask;
       hFilter.filter(_img_cv, hue_mask, true);
       sFilter.filter(_img_cv, sat_mask, true);
       cv::bitwise_and(hue_mask, sat_mask, color_mask);
+      
+      // AND the color-space mask and the circular hough space mask
+      cv::Mat needle_mask;
+      cv::bitwise_and(color_mask, circular_mask, needle_mask);
 
       cv::imshow("Original", _img_cv);
       cv::waitKey(5);
       cv::imshow("Hue and Saturation Filtered", color_mask);
       cv::waitKey(5);
-      cv::imshow("Hough Circles: ANDed", circular_mask);
+      cv::imshow("Needle Mask", needle_mask);
       cv::waitKey(5);
     }
   }
 
 public:
   NeedleFinder(ros::NodeHandle * nh_ptr,
-	       std::string cloud_topic="/camera/depth_registered/points") 
-    : CloudImageComm(nh_ptr, cloud_topic) { }
+	       std::string cloud_topic="/camera/depth_registered/points",
+	       uint8_t h_min=75, uint8_t h_max=90,
+	       uint8_t s_min=50, uint8_t s_max=255) 
+    : CloudImageComm(nh_ptr, cloud_topic),
+      cannyblur(new CannyBlur),
+      ander(cannyblur, 3),
+      hFilter(h_min, h_max),
+      sFilter(s_min, s_max){ }
 };
 
 
 int main(int argc, char** argv) {
-
   ros::init(argc, argv, "and_image_node");
   ros::NodeHandle nh("~");
 
   std::string topic; 
-  nh.param<std::string>("cloud_topic", topic, "/camera/depth_registered/points");
+  nh.param<std::string>("cloud_topic", topic, 
+			"/camera/depth_registered/points");
   NeedleFinder n_finder(&nh, topic);
 
   ros::spin();
