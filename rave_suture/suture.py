d2f5127724280fad5d180b06209a27cfe67da581
Ankush Gupta <ankush.me@hotmail.com>
2012-11-04 01:16:07 -0800

    openrave suture.py ik move with collision checking done.

--- rave_suture/suture.py
+++ rave_suture/suture.py
@@ -9,12 +9,24 @@ env.Load('/home/ankush/sandbox/rave_suture/suture_env.zae')
 robot   = env.GetRobots()[0]
 cloth   = env.GetBodies()[1]
 table   = env.GetBodies()[2]
-    
+
+# get the cloth trimesh location
+cloth_aabb = cloth.ComputeAABB()
+h1         = env.plot3(cloth_aabb.pos(), 10) # plot one point
+
 RaveSetDebugLevel(DebugLevel.Debug)
 
+for manip in robot.GetManipulators():
+    print manip.GetName()
 
+# Get/generate the ik database for leftarm+torso manipulator
+robot.SetActiveManipulator('rightarm_torso')
 manip = robot.GetActiveManipulator()
-ikmodel = databases.inversekinematics.InverseKinematicsModel(robot,iktype=IkParameterization.Type.Translation3D)
+print("---------------------")
+print manip.GetName()
+print("---------------------")
+ikmodel = databases.inversekinematics.InverseKinematicsModel(robot,
+                                                             iktype=IkParameterization.Type.Transform6D)
 print("Getting the ik database..")
 if not ikmodel.load():
     print("Generating pr2 ik database.")
@@ -22,33 +34,26 @@ if not ikmodel.load():
 else:
     print("Found ik database.")
 
-with robot: # lock environment and save robot state
-    robot.SetDOFValues([2.58, 0.547, 1.5, -0.7],[0,1,2,3]) # set the first 4 dof values
-    Tee = manip.GetEndEffectorTransform() # get end effector
-    ikparam = IkParameterization(Tee[0:3,3],ikmodel.iktype) # build up the translation3d ik query
-    sols = manip.FindIKSolutions(ikparam, IkFilterOptions.CheckEnvCollisions) # get all solutions
+goal_transform = manip.GetEndEffectorTransform()
+#goal_transform[0,3] -= 0.05# np.array(cloth_aabb.pos())
+#goal_transform[2,3] += 0.1# np.array(cloth_aabb.pos())
+goal_transform[0:3,3] = cloth_aabb.pos()
+goal_transform[2,3]   += 0.03
+env.plot3(goal_transform[0:3,3], 100) # plot one point
 
-h = env.plot3(Tee[0:3,3],10) # plot one point
-with robot: # save robot state
-    raveLogInfo('%d solutions'%len(sols))
-    for sol in sols: # go through every solution
-        robot.SetDOFValues(sol,manip.GetArmIndices()) # set the current solution
-        env.UpdatePublishedBodies() # allow viewer to update new robot
-        time.sleep(10.0/len(sols))
+#print goal_transform
+#print manip.GetTransform()
 
-raveLogInfo('restored dof values: '+repr(robot.GetDOFValues())) # robot state is restored to original
+ikparam = IkParameterization(goal_transform,IkParameterizationType.Transform6D)
+sol = manip.FindIKSolution(ikparam, IkFilterOptions.CheckEnvCollisions)
 
-"""
-pr2.SetActiveManipulator('leftarm_torso') # set the manipulator to leftarm + torso
-ikmodel = databases.inversekinematics.InverseKinematicsModel(pr2,iktype=IkParameterization.Type.Transform6D)
-if not ikmodel.load():
-    ikmodel.autogenerate()
-
-manipprob = interfaces.BaseManipulation(pr2) # create the interface for basic manipulation programs
-Tgoal = numpy.array([[0,-1,0,-0.21],[-1,0,0,0.04],[0,0,-1,0.92],[0,0,0,1]])
-res = manipprob.MoveToHandPosition(matrices=[Tgoal],seedik=10) # call motion planner with goal joint angles
-pr2.WaitForController(0) # wait
-"""
+if sol is not None:
+    robot.SetDOFValues(sol,manip.GetArmIndices())
+    print("!!!!!!!!!!! moved !!!!!!!!!!!!!")
+else:
+    print("***** No IK solution found *****")
+        
 
 while True:
+    #print("\tsleeping..")
     time.sleep(0.01)
